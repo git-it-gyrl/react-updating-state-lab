3869c63b8e229457eea6286e80ba3c9228926dc5
celeste.tech@icloud.com <celeste.tech@icloud.com>
2021-04-28 13:48:26 -0500

    commit tree 60ee294a33b80aebf292c6cff14ef8e1906f7cbd

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,25 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+          }
+
+    }
+        render() {
+            return(<div></div>)
+
+        }
+}
+export default YouTubeDebugger;
